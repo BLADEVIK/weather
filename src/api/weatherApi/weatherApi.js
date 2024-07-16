1ba2cca63df323168327de7dfe532b51aa5d10a5
Viktor Pirogov <Blade-85@inbox.ru>
2024-07-16 11:47:33 +0300

    Функционал

--- src/api/weatherApi/weatherApi.js
+++ src/api/weatherApi/weatherApi.js
@@ -1,7 +1,8 @@
-export default function WeatherApi({city}) {
+export default function WeatherApi({ city }) {
   return fetch(
-    `https://api.weatherapi.com/v1/current.json?key=e34759cd105b48cea3775556241507&q=${city}&aqi=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=e34759cd105b48cea3775556241507&days=6&q=${city}`
   ).then((res) => {
-    console.log(res.json());
+    return res.json();
   });
 }
+
--- src/componets/cardItem/cardItem.jsx
+++ src/componets/cardItem/cardItem.jsx
@@ -2,23 +2,49 @@ import { useEffect, useState } from "react";
 import WeatherApi from "../../api/weatherApi/weatherApi";
 
 export default function CardItem() {
-  const [search, setSearch] = useState("Moscow");
-  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState(null);
   useEffect(() => {
+    if (search.length < 3) {
+      setData(null);
+      return;
+    }
+
     WeatherApi({ city: search }).then((res) => {
       setData(res);
     });
   }, [search]);
   console.log(data);
+  const handleSearshInput = (event) => {
+    setSearch(event.target.value);
+  };
   return (
     <>
       <h1>Прогноз погоды города</h1>
       <input
-        onChange={(event) => setSearch(event.target.value)}
+        onChange={handleSearshInput}
         placeholder="Введите город"
         type="text"
       />
-      <ul>{data && data.map((el) => <li key={el}>{el}</li>)}</ul>
+
+      {data ? (
+        <div className="weather">
+          <h1 className="weather__city">Город: {data.location.name}</h1>
+          {data.forecast.forecastday.map((el) => (
+            <div key={el.date_epoch}>
+              <h2>
+                Температура: {el.day.avgtemp_c}°{" "}
+                <img src={el.day.condition.icon} />
+              </h2>
+              <p>Скорость ветра: {el.day.maxwind_kph} kph</p>
+              <p>Влажность: {el.day.avghumidity}</p>
+              <p>Дата: {el.date}</p>
+            </div>
+          ))}
+        </div>
+      ) : search.length < 3 ? null : (
+        "Загрузка"
+      )}
     </>
   );
 }
