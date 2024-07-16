408f26c558590abafad5433041c13e7c470e292b
Viktor Pirogov <Blade-85@inbox.ru>
2024-07-16 14:12:37 +0300

    Верстка и обработка ошибок поля инпут

--- src/api/weatherApi/weatherApi.js
+++ src/api/weatherApi/weatherApi.js
@@ -2,7 +2,10 @@ export default function WeatherApi({ city }) {
   return fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=e34759cd105b48cea3775556241507&days=6&q=${city}`
   ).then((res) => {
-    return res.json();
+    if (res.status === 400) {
+      alert("Нет такого города");
+    } else {
+      return res.json();
+    }
   });
 }
-
--- src/componets/cardItem/cardItem.jsx
+++ src/componets/cardItem/cardItem.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import WeatherApi from "../../api/weatherApi/weatherApi";
+import "./cardItem.css";
 
 export default function CardItem() {
   const [search, setSearch] = useState("");
@@ -20,8 +21,9 @@ export default function CardItem() {
   };
   return (
     <>
-      <h1>Прогноз погоды города</h1>
+      <h1 className="weather__city_title">Прогноз погоды города</h1>
       <input
+        className="weather__city_input"
         onChange={handleSearshInput}
         placeholder="Введите город"
         type="text"
@@ -31,13 +33,11 @@ export default function CardItem() {
         <div className="weather">
           <h1 className="weather__city">Город: {data.location.name}</h1>
           {data.forecast.forecastday.map((el) => (
-            <div key={el.date_epoch}>
-              <h2>
-                Температура: {el.day.avgtemp_c}°{" "}
-                <img src={el.day.condition.icon} />
-              </h2>
+            <div className="weather__card" key={el.date_epoch}>
+              <img src={el.day.condition.icon} />
+              <h2>Температура: {el.day.avgtemp_c} ° </h2>
               <p>Скорость ветра: {el.day.maxwind_kph} kph</p>
-              <p>Влажность: {el.day.avghumidity}</p>
+              <p>Влажность: {el.day.avghumidity} %</p>
               <p>Дата: {el.date}</p>
             </div>
           ))}
