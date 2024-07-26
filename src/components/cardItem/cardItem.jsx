1e7abc843f77a6359a21b65f03d013b5a74f40cc
Viktor Pirogov <Blade-85@inbox.ru>
2024-07-26 18:29:20 +0300

    Статичные надписи город и регион

--- src/components/cardItem/cardItem.jsx
+++ src/components/cardItem/cardItem.jsx
@@ -28,12 +28,22 @@ export default function CardItem() {
         placeholder="Введите город"
         type="text"
       />
+      {data === null ? (
+        <h1>Регион:</h1>
+      ) : (
+        <h1>Регион: {data.location.region}</h1>
+      )}
+      {data === null ? (
+        <h1 className="weather__city">Город:</h1>
+      ) : (
+        <h1 className="weather__city">Город: {data.location.name}</h1>
+      )}
       {data ? (
         <div className="weather">
-          <h1 className="weather__city">Город: {data.location.name}</h1>
           {data.forecast.forecastday.map((el) => (
             <div className="weather__card" key={el.date_epoch}>
               <img src={el.day.condition.icon} />
+              <h3>Погода: {el.day.condition.text}</h3>
               <h2>Температура: {el.day.avgtemp_c} ° </h2>
               <p>Скорость ветра: {el.day.maxwind_kph} kph</p>
               <p>Влажность: {el.day.avghumidity} %</p>
